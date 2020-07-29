dac1bf50a6ad9a04202b338c6c57df27be7f2071
lw-classy <levinwalser@gmx.de>
2020-07-29 07:30:41 +0200

    Updates

---
+++ precache-manifest.9eddd1171218e2025a7bf39bbdc21f5f.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6141267362d0a2b0ed8dd85d7c7b080d",
+    "url": "/pemf/index.html"
+  },
+  {
+    "revision": "cd3878ea3a6012f8ec49",
+    "url": "/pemf/static/css/main.a5fa0e66.chunk.css"
+  },
+  {
+    "revision": "0b58bb7fc957183d0fb7",
+    "url": "/pemf/static/js/2.0b62db48.chunk.js"
+  },
+  {
+    "revision": "b7bdffd4fd4ff576b1a1d037e48d8a27",
+    "url": "/pemf/static/js/2.0b62db48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3878ea3a6012f8ec49",
+    "url": "/pemf/static/js/main.9e72f7b3.chunk.js"
+  },
+  {
+    "revision": "43a3b92f0eac2d27ef77",
+    "url": "/pemf/static/js/runtime-main.817d888a.js"
+  },
+  {
+    "revision": "79d3699837d4ffbc96f127c064ff8897",
+    "url": "/pemf/static/media/Arrow_skew.79d36998.svg"
+  },
+  {
+    "revision": "ddfcd2c51dedd595024788384d9e7120",
+    "url": "/pemf/static/media/Castle.ddfcd2c5.svg"
+  },
+  {
+    "revision": "dd512ac3ddbedaf687ecd74929d174f4",
+    "url": "/pemf/static/media/Castle_Desktop.dd512ac3.svg"
+  },
+  {
+    "revision": "4575f05f8fc1b2a3a5a197e98ba23fc3",
+    "url": "/pemf/static/media/Castle_iPad.4575f05f.svg"
+  },
+  {
+    "revision": "a5bb79951170f6a5d400ebe480f48a87",
+    "url": "/pemf/static/media/Castle_iPadPro.a5bb7995.svg"
+  },
+  {
+    "revision": "5e866e44a9ac129926997cdfa09be98d",
+    "url": "/pemf/static/media/Johannes.5e866e44.png"
+  },
+  {
+    "revision": "13a83dda0f99d5878ecf1dbceab7351d",
+    "url": "/pemf/static/media/Logo.13a83dda.svg"
+  },
+  {
+    "revision": "5ca1f1f1122dc617c307042f150972af",
+    "url": "/pemf/static/media/Mobile_Castle.5ca1f1f1.svg"
+  },
+  {
+    "revision": "5dd3dc4cdc5eebf5696abb3c0a8e552d",
+    "url": "/pemf/static/media/Ute.5dd3dc4c.png"
+  },
+  {
+    "revision": "3383efa206332e1a3ac8bd49aff20946",
+    "url": "/pemf/static/media/silka-black-webfont.3383efa2.woff"
+  },
+  {
+    "revision": "d5547f8cbab56adc353272c24bcf1f37",
+    "url": "/pemf/static/media/silka-black-webfont.d5547f8c.eot"
+  },
+  {
+    "revision": "e8ae896d837bb4b13ed4f7d2b177acdc",
+    "url": "/pemf/static/media/silka-black-webfont.e8ae896d.woff2"
+  },
+  {
+    "revision": "fbce246e6ad10340745ca9c85ad18fa4",
+    "url": "/pemf/static/media/silka-black-webfont.fbce246e.ttf"
+  },
+  {
+    "revision": "11defb172a6471fc41baf17f6d23b6db",
+    "url": "/pemf/static/media/silka-bold-webfont.11defb17.ttf"
+  },
+  {
+    "revision": "4dc823ef0ae2be26b58d2da11cfff37b",
+    "url": "/pemf/static/media/silka-bold-webfont.4dc823ef.woff"
+  },
+  {
+    "revision": "92147420f3f7e4c306ef7fdb73c28eea",
+    "url": "/pemf/static/media/silka-bold-webfont.92147420.eot"
+  },
+  {
+    "revision": "f28db5bde5d6f03116d2eb6c9ec50ca2",
+    "url": "/pemf/static/media/silka-bold-webfont.f28db5bd.woff2"
+  },
+  {
+    "revision": "24c940892e7d6b2bdf3397fd74329d8e",
+    "url": "/pemf/static/media/silka-extralight-webfont.24c94089.ttf"
+  },
+  {
+    "revision": "2a16f687766f7f728f5dd9cbfef241be",
+    "url": "/pemf/static/media/silka-extralight-webfont.2a16f687.woff2"
+  },
+  {
+    "revision": "8b31b66a458c207f4e140d3b40c5fe7f",
+    "url": "/pemf/static/media/silka-extralight-webfont.8b31b66a.eot"
+  },
+  {
+    "revision": "bee7e0c8155ed6953e5352a3835429c0",
+    "url": "/pemf/static/media/silka-extralight-webfont.bee7e0c8.woff"
+  },
+  {
+    "revision": "14fe37488abbada9da626d00be2fc68c",
+    "url": "/pemf/static/media/silka-light-webfont.14fe3748.woff"
+  },
+  {
+    "revision": "185efd57c530171906f46b8d5132263f",
+    "url": "/pemf/static/media/silka-light-webfont.185efd57.eot"
+  },
+  {
+    "revision": "45804845208ec9e9005e3ef1360eaffc",
+    "url": "/pemf/static/media/silka-light-webfont.45804845.woff2"
+  },
+  {
+    "revision": "bd0f6baae5bb434354569181ffd1b770",
+    "url": "/pemf/static/media/silka-light-webfont.bd0f6baa.ttf"
+  },
+  {
+    "revision": "258844b3cc24d23c8ea5641ec7b897a9",
+    "url": "/pemf/static/media/silka-medium-webfont.258844b3.woff2"
+  },
+  {
+    "revision": "bc72d32b4e86d36e82ebe8fd48f3a9ba",
+    "url": "/pemf/static/media/silka-medium-webfont.bc72d32b.ttf"
+  },
+  {
+    "revision": "d9802b80f8e38b6587760fbec1cc47d2",
+    "url": "/pemf/static/media/silka-medium-webfont.d9802b80.woff"
+  },
+  {
+    "revision": "f81e935f22460b08f78020e25f3b89b3",
+    "url": "/pemf/static/media/silka-medium-webfont.f81e935f.eot"
+  },
+  {
+    "revision": "36e009d386122b70c597799b771a0d10",
+    "url": "/pemf/static/media/silka-regular-webfont.36e009d3.woff"
+  },
+  {
+    "revision": "395c6019f11e355a3b0505a94c2e689e",
+    "url": "/pemf/static/media/silka-regular-webfont.395c6019.woff2"
+  },
+  {
+    "revision": "5e4afc29d3a90977454bd8ebd748acd6",
+    "url": "/pemf/static/media/silka-regular-webfont.5e4afc29.ttf"
+  },
+  {
+    "revision": "75cb2cca3e4ff10fde5c148bfb269e55",
+    "url": "/pemf/static/media/silka-regular-webfont.75cb2cca.eot"
+  },
+  {
+    "revision": "3853fe84f051013eae42eebf360ec5ee",
+    "url": "/pemf/static/media/silka-semibold-webfont.3853fe84.eot"
+  },
+  {
+    "revision": "56f7292614c26375c4cb455dda8a7a8f",
+    "url": "/pemf/static/media/silka-semibold-webfont.56f72926.woff2"
+  },
+  {
+    "revision": "761c0cdc9056e534ebe03c9cedb9dbfc",
+    "url": "/pemf/static/media/silka-semibold-webfont.761c0cdc.ttf"
+  },
+  {
+    "revision": "a8653936ad06ecef5495c3e8bd3d23bc",
+    "url": "/pemf/static/media/silka-semibold-webfont.a8653936.woff"
+  },
+  {
+    "revision": "0134addb2642fe19d026208e2b66feb5",
+    "url": "/pemf/static/media/silka-thin-webfont.0134addb.ttf"
+  },
+  {
+    "revision": "69b349794acce739bc3ea72af8a5661c",
+    "url": "/pemf/static/media/silka-thin-webfont.69b34979.woff"
+  },
+  {
+    "revision": "72b4d7e1bac7768ae2a507a4b90bc45c",
+    "url": "/pemf/static/media/silka-thin-webfont.72b4d7e1.eot"
+  },
+  {
+    "revision": "d83f2e2d5e46d50d28ba4ff970be7940",
+    "url": "/pemf/static/media/silka-thin-webfont.d83f2e2d.woff2"
+  }
+]);
